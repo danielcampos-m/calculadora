1b96f8511081051deb31b1e149e262b069bbba30
danielcampos-m <d.camposmartins99@gmail.com>
2021-03-30 15:04:40 -0300

    layout buttons

--- app/src/main/java/com/example/calculator/MainActivity.kt
+++ app/src/main/java/com/example/calculator/MainActivity.kt
@@ -1,15 +1,29 @@
 package com.example.calculator
 
-import android.os.Bundle
-import android.support.wearable.activity.WearableActivity
+//import android.support.wearable.activity.WearableActivity
+import android.widget.Button
+import android.widget.TextView
+import android.widget.Toast
+import android.os.Bundle as OsBundle
+import androidx.appcompat.app.AppCompatActivity
 
-class MainActivity : WearableActivity() {
+class MainActivity : AppCompatActivity() {
 
-    override fun onCreate(savedInstanceState: Bundle?) {
+    override fun onCreate(savedInstanceState: OsBundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        //Change view text
+        val botao: Button = findViewById(R.id.button)
+        val textbox: TextView = findViewById(R.id.textView4)
+        botao.setOnClickListener {
+            textbox.setText("Trocando texto...")
+            val toast = Toast.makeText(this, "Botão clicado", Toast.LENGTH_SHORT)
+            toast.show()
+
+        }
+
         // Enables Always-on
-        setAmbientEnabled()
+//        setAmbientEnabled()
     }
 }
